06031379a08ca4f712d48b2e5f6bf298d3af393d
mubarak23 <mubarakaminu340@gmail.com>
2025-07-14 10:39:30 +0100

    chore:fix rebase

--- src/http/types.rs
+++ src/http/types.rs
@@ -57,7 +57,17 @@ pub struct AssignSupportTicketRequest {
 }
 
 #[derive(Debug, Serialize, Deserialize)]
+
 pub struct ClosedProjectRequest {
     pub project_id: String,
     pub owner_address: String,
+}
+
+pub struct AllocateBountyRequest {
+    pub wallet_address: String,
+    pub project_contract_address: String,
+    pub amount: BigDecimal,
+    pub currency: String,
+    pub bounty_expiry_date: Option<DateTime<Utc>>, // ISO8601 string
+
 }
